7bf07418e6c93c5cb65cbdc146d343962081ed62
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2024-01-12 00:42:45 +0100

    Update func.py

--- src/func.py
+++ src/func.py
@@ -5,8 +5,8 @@ from sentence_transformers import SentenceTransformer
 class SentenceTransformerEmbeddings(Expression):
     '''We support all models from https://www.sbert.net/docs/pretrained_models.html#model-overview'''
 
-    def __init__(self, model='all-mpnet-base-v2'):
-        super().__init__()
+    def __init__(self, model='all-mpnet-base-v2', **kwargs):
+        super().__init__(**kwargs)
         self.model_name = model
         self.model      = SentenceTransformer(model)
 
